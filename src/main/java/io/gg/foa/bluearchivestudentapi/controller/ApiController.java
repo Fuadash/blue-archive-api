92617e0b66a826a198060972a1aafd4feb81107f
Fua <zero-geo@hotmail.co.uk>
2024-08-06 10:07:36 +0100

    small fixes + /givenName/{name} endpoint

--- src/main/java/io/gg/foa/bluearchivestudentapi/controller/ApiController.java
+++ src/main/java/io/gg/foa/bluearchivestudentapi/controller/ApiController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/student")
@@ -23,7 +24,7 @@ public class ApiController {
     private DataController dataController; // Lets Spring handle the dependency
 
     @GetMapping(value = "", produces = "application/json")
-    public String getAllStudents() {
+    public String getAllStudents() { // TODO: Why does this return a String but the other endpoint returns a ResponseEntity?
         return dataController.getStudentData();
     }
 
@@ -40,11 +41,35 @@ public class ApiController {
                         return ResponseEntity.status(HttpStatus.OK).body(objectMapper.writeValueAsString(student)); // Converts java object to json format
                     } catch (JsonProcessingException e) {
                         e.printStackTrace();
-                        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error processing student data"); // TODO: Use error ResponseEntity to return a error responses in json format
+                        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error processing student data");
                     }
                 })
                 .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).body("Student not found"));
     }
 
+    @GetMapping(value = "/givenName/{name}", produces = "application/json")
+    public ResponseEntity<String> getStudentsByGivenName(@PathVariable String name) throws JsonProcessingException {
+        String rawStudentData = dataController.getStudentData();
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<StudentRoot> responseObject = objectMapper.readValue(rawStudentData, new TypeReference<>() {});
+        List<StudentRoot> studentMatch = responseObject.stream() // Get the matching students from the name
+                .filter(student -> name.equals(student.getPersonalName())).collect(Collectors.toList());
+        if (!studentMatch.isEmpty()) {
+            try {
+                return ResponseEntity.status(HttpStatus.OK).body(objectMapper.writeValueAsString(studentMatch)); // Converts java object to json format
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error processing student data");
+            }
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Student not found");
+        }
+    }
+
     // TODO: Many more endpoints
+    // TODO: Consider having it break more elegantly when out of sync with Schale DB for whatever reason
+    // TODO: For the above, maybe a local copy of students.json based on the last successful request would be useful
+    // TODO: More robust logging
+    // TODO: Some tests, also look up what kind of tests are usually written for APIs
 }
